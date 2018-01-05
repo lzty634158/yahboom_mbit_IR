16b790145ded612438dec89d54816ea26ff485bb
lzty634158 <398310535@qq.com>
2018-01-05 21:16:13 +0800

    add IR other button

--- shims.d.ts
+++ shims.d.ts
@@ -10,14 +10,14 @@ declare namespace Mbit_IR {
      * button pushed.
      */
     //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=IR::onPressEvent
+    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=IR::init
+    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
     function init(pin: Pins): void;
 }
 
